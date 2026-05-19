260fe9f063333f552f8ebf94a413beb86283be99
Yashhh <yashraj1416@gmail.com>
2026-05-19 09:06:11 +0530

    Updated config for google auth redirect uri

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb+srv://user:password@cluster
 
 GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "YOUR_GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "YOUR_GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
+GOOGLE_REDIRECT_URI: str = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
 
 CLOUDINARY_CLOUD_NAME: str = os.getenv("CLOUDINARY_CLOUD_NAME", "")
 CLOUDINARY_API_KEY: str = os.getenv("CLOUDINARY_API_KEY", "")
